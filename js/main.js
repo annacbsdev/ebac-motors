a31251cd704967609efcc59ebbe766218fa13355
Anna Claudia <annaclaudiadev24@gmail.com>
2024-03-04 18:00:16 -0300

    commit inicial

---
+++ js/main.js
@@ -0,0 +1,63 @@
+$(document).ready(function () {
+    $('#carousel-images').slick({
+        autoplay: true
+    })
+
+    $('.menu-hamburguer').click(function () {
+        $('nav').slideToggle()
+    })
+
+    $('#telefone').mask('(00) 00000-0000')
+    $('#nascimento').mask('00/00/0000', {placeholder: '__/__/____'})
+    //$('#placa-carro').mask('SSS-0000')
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email:{
+                required: true,
+                email:true
+            },
+            telefone:{
+                required: true
+            },
+            mensagem:{
+                required: true
+            },
+            veiculoInteresse:{
+                required: false
+            }
+        },
+        messages: {
+            nome: 'Por favor, insira o seu nome.',
+            telefone: 'Por favor, insira o seu telefone.',
+            email: 'Por favor, insira o seu e-mail.'
+        },
+        submitHandler: function(form){
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador){
+            let camposIncorretos = validador.numberOfInvalids();
+            if(camposIncorretos){
+                alert(`existem ${camposIncorretos} campo(s) incompleto(s).`)
+            }
+        }
+    })
+
+    $('.lista-veiculos button').click(function(){
+        const destino = $('#contato')
+        const nomeVeiculo = $(this).parent().find('h3').text()
+
+        $('#veiculo-interesse').val(nomeVeiculo)
+
+        $('html').animate({
+            scrollTop: destino.offset().top
+        }, 1000)
+    })
+
+
+
+
+})
